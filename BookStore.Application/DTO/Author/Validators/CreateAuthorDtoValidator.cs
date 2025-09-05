27ac34458a9e241960e6732dfe484fbd5986864c
miz-ab <mizaby19@gmail.com>
2025-09-05 13:30:51 +0300

    add fluent validator

---
+++ BookStore.Application/DTO/Author/Validators/CreateAuthorDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.DTO.Author.Validators
+{
+    public class CreateAuthorDtoValidator : AbstractValidator<AuthorCreateDto>
+    {
+        public CreateAuthorDtoValidator()
+        {
+            RuleFor(p => p.FirstName)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.");
+        }
+    }
+}
