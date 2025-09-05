03b38f0035cc03e9f66fff81a9099404c1977374
miz-ab <mizaby19@gmail.com>
2025-09-05 14:05:41 +0300

    Add Repostory project

---
+++ BookStore.persistent/BookStoreDbContext.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.persistent
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext()
+        {
+        }
+
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC072338115F");
+
+                entity.Property(e => e.Bio).HasMaxLength(250);
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            //OnModelCreatingPartial(modelBuilder);
+        }
+
+        //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ BookStore.persistent/Repositories/GenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using BookStore.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.persistent.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly BookStoreDbContext context;
+        private readonly IMapper mapper;
+
+        public GenericRepository(BookStoreDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await GetAsync(id);
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
