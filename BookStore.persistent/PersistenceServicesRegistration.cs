0de5292b9bfeadd42d3cb4f980ad3b6e5cf29eba
miz-ab <mizaby19@gmail.com>
2025-09-05 16:18:45 +0300

    Add Persistent

---
+++ BookStore.persistent/PersistenceServicesRegistration.cs
@@ -0,0 +1,30 @@
+﻿using BookStore.Application.Contracts.Persistence;
+using BookStore.persistent.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.persistent
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BookStoreDbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("BookStoreAppDbConnection")));
+
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IAuthorsRepository, AuthorsRepository>();
+            
+
+            return services;
+        }
+    }
+}
---
+++ BookStore.persistent/Repositories/AuthorRepository.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BookStore.Application.Contracts.Persistence;
+using BookStore.Application.DTO.Author;
+using BookStore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.persistent.Repositories
+{
+    public class AuthorsRepository : GenericRepository<Author>, IAuthorsRepository
+    {
+        private readonly BookStoreDbContext context;
+        private readonly IMapper mapper;
+
+        public AuthorsRepository(BookStoreDbContext context, IMapper mapper) : base(context, mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public Task<Author> GetAuthorDetailsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
