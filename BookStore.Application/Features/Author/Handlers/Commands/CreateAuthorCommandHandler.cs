e82b166ec7fca016156ea328805581ab91fadce9
miz-ab <mizaby19@gmail.com>
2025-09-05 12:38:09 +0300

    Add CreateAuthorCommand and AuthorCommandRequest

---
+++ BookStore.Application/Features/Author/Handlers/Commands/CreateAuthorCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using BookStore.Application.Contracts.Persistence;
+using BookStore.Application.Features.Author.Requests.Commands;
+using BookStore.Application.Responses;
+using BookStore.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Features.Author.Handlers.Commands
+{
+    public class CreateAuthorCommandHandler : IRequestHandler<CreateAuthorCommandRequest, BaseCommandResponse>
+    {
+        private readonly IAuthorsRepository _authorsRepository;
+        private readonly IMapper _mapper;
+
+        public CreateAuthorCommandHandler(IAuthorsRepository authorsRepository, IMapper mapper)
+        {
+            _authorsRepository = authorsRepository;
+            _mapper = mapper;
+        }
+        public async Task<BaseCommandResponse> Handle(CreateAuthorCommandRequest request, CancellationToken cancellationToken)
+        {
+            var response = new BaseCommandResponse();
+
+            var authorEitity = _mapper.Map<BookStore.Domain.Author>(request.AuthorCreateDto);
+            var author = _authorsRepository.AddAsync(authorEitity);
+
+            response.Success = true;
+            response.Message = "Creation Successful";
+            response.Id = author.Id;
+            return response;
+        }
+    }
+}
---
+++ BookStore.Application/Features/Author/Requests/Commands/CreateAuthorCommandRequest.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Application.DTO.Author;
+using BookStore.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Features.Author.Requests.Commands
+{
+    public class CreateAuthorCommandRequest : IRequest<BaseCommandResponse>
+    {
+        public AuthorCreateDto AuthorCreateDto { get; set; }
+    }
+}
