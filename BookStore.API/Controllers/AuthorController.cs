0516ef2ebc176e38abbfa52829eeb4de76346623
miz-ab <mizaby19@gmail.com>
2025-09-06 12:33:55 +0300

    Add Author controller get all author endpoint

---
+++ BookStore.API/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Application.DTO.Author;
+using BookStore.Application.Features.Author.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AuthorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuthorReadOnlyDto>>> GetAuthors()
+        {
+            var authors = await _mediator.Send(new GetAuthorListRequest());
+            return authors;
+        }
+    }
+}
